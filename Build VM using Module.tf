6309ae54e6c8cc205d1b221b860e70b0d4c5f3be
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2022-03-15 13:39:26 +0000

    Update Build VM using Module.tf

--- Build VM using Module.tf
+++ Build VM using Module.tf
@@ -1,7 +1,7 @@
 
-module "WebServer2" {
+module "Build VM using Module" {
   
-  source =  "git::https://github.com/dgaharwar/MorpheusDev1"
+  source =  "git::https://github.com/dgaharwar/WebServer2.git"
 
   server_name   = "DGGITSERVER"
   server_ip     = "10.32.20.118"
